7b3fde48ad0f7b9b562aa402ac5a591fbd885bdc
shanehckr <kearldelapaz@gmail.com>
2026-05-04 01:33:45 +0800

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100 p-8">
       <header className="mb-8">
-        <h1 className="text-3xl font-bold text-slate-800">Hikarizz</h1>
-        <p className="text-slate-600">Let's go ppl</p>
+        <h1 className="text-3xl font-bold text-slate-800">Hikarizz Hooray!</h1>
+        <p className="text-slate-600">Let's get started!</p>
       </header>
     </div>
   )
